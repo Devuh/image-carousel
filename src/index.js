8720b217eb3c97458997e90be2abe178a02609e5
Devuh <david.walter.reichert@gmail.com>
2025-06-27 16:49:46 -0500

    Make gallery auto-move on timer

--- src/index.js
+++ src/index.js
@@ -8,6 +8,21 @@ images.children[0].classList.toggle("active");
 let currentImage = 0;
 navigator.children[0].classList.toggle("active");
 
+let userInteracted = false;
+
+setInterval(moveImage, 2000);
+
+function moveImage() {
+    if(!userInteracted) {
+        images.children[currentImage].classList.toggle("active");
+        navigator.children[currentImage].classList.toggle("active");
+        currentImage == 3 ? currentImage = 0 : currentImage++;
+        images.children[currentImage].classList.toggle("active");
+        navigator.children[currentImage].classList.toggle("active");
+    }
+}
+
+
 body.addEventListener("click", (event) => {
     if(event.target.id == "left") {
         images.children[currentImage].classList.toggle("active");
@@ -15,6 +30,7 @@ body.addEventListener("click", (event) => {
         currentImage == 0 ? currentImage = images.children.length - 1 : currentImage--;
         images.children[currentImage].classList.toggle("active");
         navigator.children[currentImage].classList.toggle("active");
+        userInteracted = true;
     }
 
     if(event.target.id == "right") {
@@ -23,6 +39,7 @@ body.addEventListener("click", (event) => {
         currentImage == 3 ? currentImage = 0 : currentImage++;
         images.children[currentImage].classList.toggle("active");
         navigator.children[currentImage].classList.toggle("active");
+        userInteracted = true;
     }
 
     if(event.target.nodeName == "SPAN") {
@@ -31,5 +48,6 @@ body.addEventListener("click", (event) => {
         images.children[event.target.id].classList.toggle("active");
         navigator.children[event.target.id].classList.toggle("active");
         currentImage = event.target.id;
+        userInteracted = true;
     }
 });
